5f1c16e914857c944ffb359bf11cfbed053ddbd1
doan202 <doan202mta@gmail.com>
2023-07-10 00:36:20 +0700

    lan 1

---
+++ Student/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sinhVien sv= new sinhVien();
+            sv.nhap();
+            Console.WriteLine();
+            sv.xuat();
+            sv.running();
+        }
+    }
+}
---
+++ Student/sinhVien.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class sinhVien
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public int id { get; set; }
+     public void nhap()
+        {
+            Console.WriteLine("nhap ten");
+            name= Console.ReadLine();
+            Console.WriteLine("nhap tuoi");
+            age=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("nhap ma sinh vien");
+            id=Convert.ToInt32(Console.ReadLine());
+        }
+     public void xuat()
+        {
+            Console.WriteLine(name); 
+            Console.WriteLine(age); 
+            Console.WriteLine(id); 
+            Console.WriteLine();
+        }
+    public void running()
+        {
+            Console.WriteLine("nguoi chay nhanh nhat ten la " + name);
+        }
+    }
+}
